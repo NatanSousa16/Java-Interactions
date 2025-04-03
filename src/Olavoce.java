6c664be5c1eb552c6bc34dad5bb51e9941b60745
Natan <natanslsousa.16@gmail.com>
2025-04-03 04:32:31 -0300

    Pequena Conversa com a Maquina (Incompleto)

--- src/Olavoce.java
+++ src/Olavoce.java
@@ -1,5 +1,17 @@
-public class Olavoce {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Olá Mundo!");
+import java.util.Scanner;
+public class OlaMundo {
+    public static void main(String[] args) throws InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Olá, Mundo!");
+        Thread.sleep(2000);
+        System.out.println("Ehhh... Desculpe te chamar de ... Mundo");
+        Thread.sleep(2000);
+        System.out.println("Acabei de Acordar");
+        Thread.sleep(2000);
+        System.out.println("Qual seu nome?");
+        String nome = scanner.nextLine();
+        System.out.println("A é um prazer lhe conhecer, então " + nome + " oque o traz aqui?");
+
     }
 }
